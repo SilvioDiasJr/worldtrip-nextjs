523940bacd0f041a2014ddaade385b5f380427b1
Silvio <silvio.dias@etec.sp.gov.br>
2021-09-25 04:04:26 -0300

    #refactor - adding colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,10 +2,15 @@ import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
   colors: {
+    white: '#ffffff',
+    yellow_dark: '#FFBA08',
+    yellow_light: 'rgba(255, 186, 8, 0.5)',
+
     background: "#F5F8FA",
     headings_light: "#F5F8FA", 
     headings_dark: "#47585B",
-    info_light: "#DADADA" 
+    info_light: "#DADADA", 
+    info_dark: "#999999"
   },
   fonts: {
     heading: "Poppins",
