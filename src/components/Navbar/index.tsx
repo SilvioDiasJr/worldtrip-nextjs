c80d8cd48b48e083090a2c4e3543202fa8b5cf7d
Silvio <silvio.dias@etec.sp.gov.br>
2021-09-25 03:59:54 -0300

    refactor - adding back button to home page

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,11 @@
-import { Flex, Image } from '@chakra-ui/react'
+import { Flex, Image, Link as ChakraLink } from '@chakra-ui/react'
+import Link from 'next/link'
 
-export const Navbar = () => {
+interface Props {
+  goBack?: boolean
+}
+
+export const Navbar: React.FC<Props> = ({ goBack }) => {
   return (
     <Flex
       as='nav'
@@ -9,7 +14,21 @@ export const Navbar = () => {
       align='center'
       justify='center'
     >
-      <Image src={'./assets/logo.png'} alt="Logo Worldtrip" />
+      <Flex
+        align='center'
+        justify='center'
+        maxWidth={1440}
+        paddingX='140'
+      >
+        {goBack &&
+          <Link href='/' passHref>
+            <ChakraLink position='absolute' left='140'>
+              <Image src={'./assets/icons/arrow-left.svg'} alt='Icone de flecha' />
+            </ChakraLink>
+          </Link>
+        }
+        <Image src={'./assets/logo.png'} alt="Logo Worldtrip" />
+      </Flex>
     </Flex>
   )
 }
